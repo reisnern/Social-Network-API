64ecf78ddb2b4a6d875ae94483c97c0ef228d4a3
Nick Reisner <nreisne@wgu.edu>
2021-07-08 18:35:09 -0500

    setting up controllers

---
+++ controllers/thoughts.js
@@ -0,0 +1,23 @@
+// Require Models
+const {Thoughts, Users} = require('../models');
+
+// Set up  Controller
+const thoughts = {
+
+    // Create a new thought
+    createThoughts({params, body}, res) {
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+    },
+
+    
