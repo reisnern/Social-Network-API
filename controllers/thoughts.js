128922aca928b4c9e9d2e2cffd02cccbcec3a963
Nick Reisner <nreisne@wgu.edu>
2021-07-08 18:49:55 -0500

    Working on Controllers

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -20,4 +20,47 @@ const thoughts = {
         .catch(err => res.json(err)); 
     },
 
-    
+    // Get all available Thoughts
+    getAllThoughts(req,res) {
+        Thoughts.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+  // Get thought by ID
+  getThoughtsById({params}, res) {
+    Thoughts.findOne({ _id: params.id })
+    .populate({path: 'reactions',select: '-__v'})
+    .select('-__v')
+    .then(dbThoughtsData => {
+        if(!dbThoughtsData) {
+        res.status(404).json({message: 'No thoughts exist with this ID!'});
+        return;
+    }
+    res.json(dbThoughtsData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+},
+
+// Delete thought by ID
+deleteThoughts({params}, res) {
+    Thoughts.findOneAndDelete({_id: params.id})
+    .then(dbThoughtsData => {
+        if (!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts exist with this ID!'});
+            return;
+        }
+        res.json(dbThoughtsData);
+        })
+        .catch(err => res.status(400).json(err));
+},
+
